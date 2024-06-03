88c84e3f755e869fad7a8de029a83941ff685b89
Javier Bonet <javbonet@gmail.com>
2024-06-03 20:04:15 -0300

    Removes file-loader from webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -41,10 +41,6 @@ module.exports = {
         test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: ['file-loader'],
-      },
     ],
   },
 };
